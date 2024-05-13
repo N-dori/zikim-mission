43b666916c48e09444ae172ecb5781c9c65df92d
Nadav Dori <dori.nadav@gmail.com>
2024-05-13 14:59:33 +0300

    trying to build

--- src/app/api/images/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server"
-
-export async function GET () {
-  const images = await fetch(`https://api.unsplash.com/search/photos/?query=girls&client_id=15mtyMgdr5wQ4UkJrz17VIF5R42qQbPKQhxpfx2DZ_g`)
-console.log('images',images);
-  return  NextResponse.json({images})
-}
