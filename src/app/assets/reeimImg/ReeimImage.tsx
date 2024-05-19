93a06e3b93246e0a10fd5b616d8d0229470a5570
Nadav Dori <dori.nadav@gmail.com>
2024-05-19 17:46:04 +0300

    fixed writing errors and bad imgs7

--- src/app/assets/reeimImg/ReeimImage.tsx
+++ src/app/assets/reeimImg/ReeimImage.tsx
@@ -7,11 +7,15 @@ type Props = {}
 export default function ReeimImage({}: Props) {
   return (
     <>
-    <Image className="Image" width={150} height={150} src="/reeim_memorial.png " alt="memorial for the masecere in reeim" onError={({ currentTarget }) => {
-        currentTarget.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/101894_reeim_-_memorial_PikiWiki_Israel.jpg/800px-101894_reeim_-_memorial_PikiWiki_Israel.jpg";
+    <Image className="Image" width={150} height={150} 
+    src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/101894_reeim_-_memorial_PikiWiki_Israel.jpg/800px-101894_reeim_-_memorial_PikiWiki_Israel.jpg" 
+    alt="memorial for the masecere in reeim" 
+    onError={({ currentTarget }:any) => {
+        currentTarget.src = "/reeim_memorial.png ";
         currentTarget.onerror = null; // prevents looping
-    }} />
-    <span className="Image-desc">מתחם הטבח ברעים ההיסוריה חוזרת על עצמה</span>  )
+    }} 
+    />
+    <span className="Image-desc">מתחם הטבח ברעים ההיסוריה חוזרת על עצמה</span>  
     
     </>)
 }
