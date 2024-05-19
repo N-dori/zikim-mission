2ea4da1112a43b5794e78e19ac5458378cc5249c
Nadav Dori <dori.nadav@gmail.com>
2024-05-19 17:51:26 +0300

    fixed writing errors and bad imgs7

--- src/app/assets/reeimImg/ReeimImage.tsx
+++ src/app/assets/reeimImg/ReeimImage.tsx
@@ -1,4 +1,4 @@
-'use client'
+
 import Image from 'next/image';
 import React from 'react'
 
@@ -8,12 +8,8 @@ export default function ReeimImage({}: Props) {
   return (
     <>
     <Image className="Image" width={150} height={150} 
-    src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/101894_reeim_-_memorial_PikiWiki_Israel.jpg/800px-101894_reeim_-_memorial_PikiWiki_Israel.jpg" 
-    alt="memorial for the masecere in reeim" 
-    onError={({ currentTarget }:any) => {
-        currentTarget.src = "/reeim_memorial.png ";
-        currentTarget.onerror = null; // prevents looping
-    }} 
+    src= "/reeim_memorial.png"
+    alt="memorial for the masecere in reeim"  
     />
     <span className="Image-desc">מתחם הטבח ברעים ההיסוריה חוזרת על עצמה</span>  
     
