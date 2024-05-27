59c247b2f9d5cdebdf0328ae75e4d5e85932439b
Nadav Dori <dori.nadav@gmail.com>
2024-05-27 12:45:44 +0300

    added ui to login signup froms

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import { redirect } from 'next/navigation'
 import Link from "next/link";
 export default async function Home() {
   
   const session =await getServerSession(authOptions)
   console.log('session',session);
  
-  
+  if(session){
+    redirect(`/menu`)
+  }
   return (
      <main className="intro-container gc2">
       <Image width={100} height={400} src='/hero.jpg' style={{ height: '40%' }} alt="pic" className="hero-img" layout="responsive" />
