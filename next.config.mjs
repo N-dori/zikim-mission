363af35e084a05c969f7a3f24512e41d51af8ad8
Nadav Dori <dori.nadav@gmail.com>
2024-05-28 10:23:02 +0300

    trying to fix corss issue 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    async headers() {
+        return [
+          {
+            // Apply these headers to all API routes
+            source: "/api/:path*", // This pattern matches all routes under /api
+            headers: [
+              { key: "Access-Control-Allow-Credentials", value: "true" },
+              // Allow all origins - in a production environment, replace "*" with your frontend URL for security
+              { key: "Access-Control-Allow-Origin", value: "*" },
+              // List of allowed HTTP methods
+              { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+              // List of allowed headers in the request
+              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+          }
+        ];
+      },
 };
 
 export default nextConfig;
