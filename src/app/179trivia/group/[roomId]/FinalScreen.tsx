3afd6afcfb04091715cd9311590ff40baa68ff76
Nadav Dori <dori.nadav@gmail.com>
2026-05-09 00:52:20 +0300

    fix style

--- src/app/179trivia/group/[roomId]/FinalScreen.tsx
+++ src/app/179trivia/group/[roomId]/FinalScreen.tsx
@@ -6,23 +6,24 @@ import { Tanswer, Tplayer } from '@/app/types/types'
 import ScoreTableList from './ScoreTableList'
 
 type FinalScreenProps = {
-    roomId: string
-    winHeight:any,
-    handelNewGame :()=> void
-    players: Tplayer[]
-    results:Tanswer[]
+  roomId: string
+  winHeight: any,
+  handelNewGame: () => void
+  players: Tplayer[]
+  results: Tanswer[]
 
 
 }
 
-export default function FinalScreen({roomId, players, winHeight,handelNewGame, results,}: FinalScreenProps) {
+export default function FinalScreen({ roomId, players, winHeight, handelNewGame, results, }: FinalScreenProps) {
   return (
-    <section style={{ height: winHeight.height }} className='end-of-triva-msg-conatiner flex-col flex-jc-ac'>
-    <Confetti1 />
-    <p className='end-of-triva-msg'>כל הכבוד !!!</p>
-    <ScoreTableList roomId={roomId} players={players} results={results} />
-    <Link className='no-under-line back-btn tac' href={'/'}>חזרה לעמוד הראשי</Link>
-    {/* <button type='button' className='back-btn' onClick={handelNewGame}>שחק מהתחלה</button> */}
-</section>
+    <section style={{ height: winHeight.height }} 
+     className='end-of-triva-msg-conatiner flex-col flex-jc-ac'>
+      <Confetti1 />
+      <p className='end-of-triva-msg'>כל הכבוד !!!</p>
+      <ScoreTableList roomId={roomId} players={players} results={results} />
+      <Link className='no-under-line back-btn tac' href={'/'}>חזרה לעמוד הראשי</Link>
+      {/* <button type='button' className='back-btn' onClick={handelNewGame}>שחק מהתחלה</button> */}
+    </section>
   )
 }
